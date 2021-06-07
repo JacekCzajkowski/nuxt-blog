b981763b9b95e87a10fd244f19c8dc7f08c1cea9
Jacek <ja_cek@gmx.com>
2021-06-07 15:34:01 +0200

    project update

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="post-list">
+      <PostPreview
+        id="1"
+        thumbnail="https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg" 
+        title="Hello there!"
+        previewText="This is my first post!" />
+      <PostPreview
+        id="2"
+        thumbnail="https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg" 
+        title="Hello there folks!"
+        previewText="This is my second post!" />
+      <PostPreview
+        id="3"
+        thumbnail="https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg" 
+        title="Hello dudes!"
+        previewText="This is my third post!" />
+    </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,16 @@
     <section class="intro">
       <h1>Get the latest IT news</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg" 
-        title="Hello there!"
-        previewText="This is my first post!" />
-      <PostPreview
-        id="2"
-        thumbnail="https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg" 
-        title="Hello there folks!"
-        previewText="This is my second post!" />
-      <PostPreview
-        id="3"
-        thumbnail="https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg" 
-        title="Hello dudes!"
-        previewText="This is my third post!" />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
 
 export default {
     components: {
-      PostPreview
+      PostList
     }
 }
 </script>
