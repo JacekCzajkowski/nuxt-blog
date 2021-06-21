efacda85b7190ac024f7413620b129493240e45d
Jacek <ja_cek@gmx.com>
2021-06-21 12:37:24 +0200

    project update

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Jacek',
+        title: 'Master Post',
+        content: 'Super Wow!',
+        thumbnailLink: 'https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg'
+
+      }
+    }
+  }
+}
+</script>
